40e4280db0b54f95462700469e4f4332d1700d01
glory-ych <gloryforme@163.com>
2016-10-21 10:27:29 +0800

    edit

--- src/main/java/annotation/serializable/Student.java
+++ src/main/java/annotation/serializable/Student.java
@@ -4,4 +4,13 @@ package annotation.serializable;
  * Created by yangch on 2016/9/28.
  */
 public class Student   {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
